cc17ef8ac13c767efeb45342fd4dcd71910244a4
antuneslinda0 <127418616+antuneslinda0@users.noreply.github.com>
2023-10-26 12:08:36 -0300

    Add files via upload

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,3 +1,15 @@
-document.queryselector('.tecla_pom')
+function TocaSomPom () {
+   document.querySelector('#som_tecla_pom').play();
 
-document.querySelector('#som_tecla_pom').play()
+}
+
+document.querySelector('.tecla').onclick = TocaSomPom;
+
+function TocaSomClap () {
+    document.querySelector('#som_tecla_Clap').play();
+ 
+ }
+ 
+ document.querySelectorAll('.tecla')
+ 
+ 
